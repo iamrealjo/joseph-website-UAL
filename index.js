87e8e41eeab9480aa8a677393fe6326163010928
Joseph Mosi <iamrealjo@gmail.com>
2025-07-29 22:22:44 +0100

    front page secured and full page deisgned

---
+++ index.js
@@ -0,0 +1,26 @@
+let age = 18;
+let haslicencse = true;
+
+
+
+if (age>=16) {console.log ("you are old enough to drive"); 
+
+    if (haslicencse){console.log ("you have your license")}
+
+    else {console.log ("you DO NOT have your license")}
+}
+
+else {console.log ("you must be 16+ to have a license")}
+
+
+
+//console.log(`Hello`);
+//onsole.log(`I like pizza`);
+
+
+//window.alert(`I like pizza`);
+
+//let x;
+//x=100
+
+//console.log(`You are ${x} years old`);
